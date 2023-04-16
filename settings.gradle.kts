35f92cde4db034a6252e9ce1a56cee284469226a
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-04-16 16:45:16 +1000

    Move api to separate project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "vision-gui"
+
+sequenceOf("vision-api").forEach { include(it) }
---
+++ vision-api/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.jspecify:jspecify:0.2.0")
+    // TODO: Update to 0.3.0 when more tools add support
+
+    api("net.kyori:adventure-api:4.13.1") {
+        exclude("org.jetbrains:annotations")
+    }
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.mockito:mockito-core:5.2.0")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
