becbda8d9cae52823f7cfd192ecec65e503c3db6
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-06-01 22:52:22 +1000

    Create an example plugin

---
+++ example/src/main/java/me/sparky983/example/ExamplePlugin.java
@@ -0,0 +1,22 @@
+package me.sparky983.example;
+
+import me.sparky983.vision.PaperVision;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExamplePlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+
+        final PaperVision vision = PaperVision.create(this);
+
+        final CommandExecutor testGuiCommandExecutor = new ExampleGuiCommandExecutor(vision);
+
+        final PluginCommand testGuiCommand = getCommand("example-gui");
+        assert testGuiCommand != null;
+        testGuiCommand.setExecutor(testGuiCommandExecutor);
+    }
+}
