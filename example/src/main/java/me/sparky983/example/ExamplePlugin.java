6bb981b737c3d1e8f460a8b59735fb52ab4673fd
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-06-02 23:34:39 +1000

    Unimplement `Listener` from example plugin

--- example/src/main/java/me/sparky983/example/ExamplePlugin.java
+++ example/src/main/java/me/sparky983/example/ExamplePlugin.java
@@ -3,10 +3,9 @@ package me.sparky983.example;
 import me.sparky983.vision.PaperVision;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ExamplePlugin extends JavaPlugin implements Listener {
+public class ExamplePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
