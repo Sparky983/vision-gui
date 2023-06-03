afeb5d8c2080b3b84891b3699b040b33b7036b67
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-06-03 19:03:24 +1000

    Add click type tests

--- vision-api/src/test/java/me/sparky983/vision/ClickTests.java
+++ vision-api/src/test/java/me/sparky983/vision/ClickTests.java
@@ -1,10 +1,15 @@
 package me.sparky983.vision;
 
 import net.kyori.adventure.audience.Audience;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ClickTests {
 
@@ -76,5 +81,97 @@ class ClickTests {
         assertEquals(BUTTON, click.button());
         assertEquals(SLOT, click.slot());
         assertEquals(TYPE, click.type());
+    }
+
+    @Nested
+    class TypeTests {
+
+        @ValueSource(strings = {"LEFT", "SHIFT_LEFT"})
+        @ParameterizedTest
+        void testIsLeftWhenTypeIsLeft(final Click.Type type) {
+
+            assertTrue(Click.Type.LEFT.isLeft());
+        }
+
+        @ValueSource(strings = {
+                "RIGHT",
+                "SHIFT_RIGHT",
+                "MIDDLE",
+                "DROP",
+                "CONTROL_DROP",
+                "DOUBLE_CLICK",
+                "NUMBER_KEY"
+        })
+        @ParameterizedTest
+        void testIsLeftWhenTypeIsNotLeft(final Click.Type type) {
+
+            assertFalse(Click.Type.LEFT.isLeft());
+        }
+
+        @ValueSource(strings = {"RIGHT", "SHIFT_RIGHT"})
+        @ParameterizedTest
+        void testIsRightWhenTypeIsRight(final Click.Type type) {
+
+            assertTrue(Click.Type.RIGHT.isRight());
+        }
+
+        @ValueSource(strings = {
+                "LEFT",
+                "SHIFT_LEFT",
+                "MIDDLE",
+                "DROP",
+                "CONTROL_DROP",
+                "DOUBLE_CLICK",
+                "NUMBER_KEY"
+        })
+        @ParameterizedTest
+        void testIsRightWhenTypeIsNotRight(final Click.Type type) {
+
+            assertFalse(Click.Type.RIGHT.isRight());
+        }
+
+        @ValueSource(strings = {"SHIFT_LEFT", "SHIFT_RIGHT"})
+        @ParameterizedTest
+        void testIsShiftWhenTypeIsShift(final Click.Type type) {
+
+            assertTrue(type.isShift());
+        }
+
+        @ValueSource(strings = {
+                "LEFT",
+                "RIGHT",
+                "MIDDLE",
+                "DROP",
+                "CONTROL_DROP",
+                "DOUBLE_CLICK",
+                "NUMBER_KEY"
+        })
+        @ParameterizedTest
+        void testIsShiftWhenTypeIsNotShift(final Click.Type type) {
+
+            assertFalse(type.isShift());
+        }
+
+        @ValueSource(strings = {"DROP", "CONTROL_DROP"})
+        @ParameterizedTest
+        void testIsDropWhenTypeIsDrop(final Click.Type type) {
+
+            assertTrue(type.isDrop());
+        }
+
+        @ValueSource(strings = {
+                "LEFT",
+                "RIGHT",
+                "SHIFT_LEFT",
+                "SHIFT_RIGHT",
+                "MIDDLE",
+                "DOUBLE_CLICK",
+                "NUMBER_KEY"
+        })
+        @ParameterizedTest
+        void testIsDropWhenTypeIsNotDrop(final Click.Type type) {
+
+            assertFalse(type.isDrop());
+        }
     }
 }
