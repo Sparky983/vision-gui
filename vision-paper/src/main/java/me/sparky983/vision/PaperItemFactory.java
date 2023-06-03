4a23133c70f6d932e0b2a2c1d4c2376d2646560f
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-06-04 09:28:31 +1000

    Remove unused constant

--- vision-paper/src/main/java/me/sparky983/vision/PaperItemFactory.java
+++ vision-paper/src/main/java/me/sparky983/vision/PaperItemFactory.java
@@ -1,8 +1,5 @@
 package me.sparky983.vision;
 
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.Style;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.inventory.ItemStack;
 import org.jspecify.nullness.NullMarked;
 
@@ -11,11 +8,6 @@ import org.jspecify.nullness.NullMarked;
  */
 @NullMarked
 interface PaperItemFactory {
-
-    Style DEFAULT_STYLE = Style.style()
-            .color(NamedTextColor.WHITE)
-            .decorations(TextDecoration.NAMES.values(), false)
-            .build();
 
     /**
      * Creates a new {@link ItemStack} from the specified {@link Button}.
