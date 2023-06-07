47df05891bd1a1a7e1803f9182bdf4fb4224d50c
Sparky <87631423+Sparky983@users.noreply.github.com>
2023-06-08 07:25:43 +1000

    Organize gradle project
    - Created `buildSrc` project
    - Moved shared configuration into convention plugins

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
+    compileOnly("org.jspecify:jspecify:0.2.0")
+    // TODO: Update to 0.3.0 when more tools add support
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.mockito:mockito-core:5.2.0")
+}
