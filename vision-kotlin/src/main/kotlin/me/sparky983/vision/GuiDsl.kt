fc1ec650314fe7fd7a290f61f119bcb5f8167e76
Sparky <87631423+Sparky983@users.noreply.github.com>
2023-06-08 20:16:27 +1000

    gui dsl

--- vision-kotlin/src/main/kotlin/me/sparky983/vision/GuiDsl.kt
+++ vision-kotlin/src/main/kotlin/me/sparky983/vision/GuiDsl.kt
@@ -1,15 +1,90 @@
 package me.sparky983.vision
 
 import net.kyori.adventure.text.Component
+import java.lang.IllegalStateException
 
-public inline fun gui(title: Component?, rows: Int, init: Gui.() -> Unit): Gui {
-    return Gui.gui(title, rows).apply(init)
+@DslMarker
+private annotation class GuiDslMarker
+
+@GuiDslMarker
+public interface GuiDsl {
+    public var title: Component?
+    public var rows: Int
+
+    public fun button(slot: Slot, button: Button)
+
+    public fun button(slot: Slot): Button?
 }
 
-public inline fun gui(rows: Int, init: Gui.() -> Unit): Gui {
-    return gui(title = null, rows, init)
+private class GuiDslImpl : GuiDsl {
+    public override var title: Component? = null
+    public override var rows: Int = 1
+
+    private val buttons: MutableMap<Slot, Button> = hashMapOf()
+
+    public override fun button(slot: Slot, button: Button) {
+        buttons[slot] = button
+    }
+
+    public override fun button(slot: Slot): Button? {
+        return buttons[slot]
+    }
+
+    @PublishedApi
+    internal fun build(): Gui {
+        val gui = Gui.gui(title, rows)
+
+        buttons.forEach(gui::button)
+
+        return gui
+    }
 }
 
-public inline fun Gui.button(slot: Slot, type: ItemType, init: Button.() -> Unit): Button {
-    return Button.of(type).apply(init).apply { button(slot, this) }
+public fun gui(init: GuiDsl.() -> Unit): Gui {
+    return GuiDslImpl().apply(init).build()
+}
+
+@GuiDslMarker
+public interface ButtonDsl {
+    public var type: ItemType?
+    public var name: Component?
+    public var lore: List<Component>
+    public var amount: Int
+
+    public fun onClick(handler: (Click) -> Unit)
+}
+
+private class ButtonDslImpl : ButtonDsl {
+    public override var type: ItemType? = null
+    public override var name: Component? = null
+    public override var lore: List<Component> = listOf()
+    public override var amount: Int = 1
+
+    private val clickHandlers: MutableList<(Click) -> Unit> = mutableListOf()
+
+    public override fun onClick(handler: (Click) -> Unit) {
+        clickHandlers.add(handler)
+    }
+
+    internal fun build(): Button {
+        val type = type ?:
+            throw IllegalStateException("type property must be specified and non-null")
+
+        val button = Button.of(type)
+            .name(name)
+            .lore(lore)
+            .amount(amount)
+
+        clickHandlers.forEach(button::onClick)
+
+        return button
+    }
+}
+
+public fun button(init: ButtonDsl.() -> Unit): Button {
+    return ButtonDslImpl().apply(init).build()
+}
+
+public fun GuiDsl.button(slot: Slot, init: ButtonDsl.() -> Unit) {
+    button(slot, button(init))
 }
