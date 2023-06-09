5210a914715f1e3197455abbd79587994465593e
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-06-09 19:56:28 +1000

    Update PaperVisionTests.java

--- vision-paper/src/test/java/me/sparky983/vision/PaperVisionTests.java
+++ vision-paper/src/test/java/me/sparky983/vision/PaperVisionTests.java
@@ -1,32 +1,24 @@
 package me.sparky983.vision;
 
-import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class PaperVisionTests {
 
     Plugin plugin;
-    Server server;
-    PluginManager pluginManager;
     PaperVision paperVision;
 
     @BeforeEach
     void setUp() {
 
-        plugin = mock();
-        server = mock();
-        pluginManager = mock();
-        when(server.getPluginManager()).thenReturn(pluginManager);
-        when(plugin.getServer()).thenReturn(server);
+        plugin = mock(Answers.RETURNS_MOCKS);
         paperVision = PaperVision.create(plugin);
     }
 
