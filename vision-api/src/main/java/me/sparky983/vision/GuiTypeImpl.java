3b93bc50afa7aecc7daddf826a34ed39ec2c2346
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-06-25 17:14:23 +1000

    Update GuiTypeImpl.java

--- vision-api/src/main/java/me/sparky983/vision/GuiTypeImpl.java
+++ vision-api/src/main/java/me/sparky983/vision/GuiTypeImpl.java
@@ -5,6 +5,13 @@ import org.jetbrains.annotations.VisibleForTesting;
 
 final class GuiTypeImpl {
 
+    @VisibleForTesting
+    static final String INCORRECT_ROWS = "rows must be between "
+            + GuiType.Chest.MIN_ROWS
+            + " and "
+            + GuiType.Chest.MAX_ROWS
+            + " (got %s)";
+
     private GuiTypeImpl() {
 
     }
@@ -22,13 +29,6 @@ final class GuiTypeImpl {
     }
 
     private record ChestImpl(@Override int rows) implements GuiType.Chest {
-
-        @VisibleForTesting
-        static final String INCORRECT_ROWS = "rows must be between "
-                + GuiType.Chest.MIN_ROWS
-                + " and "
-                + GuiType.Chest.MAX_ROWS
-                + " (got %s)";
 
         @VisibleForTesting
         static final Component NAME = Component.translatable("container.chest");
