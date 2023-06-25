88fb72a33c13d71176968debfc9230a4ac2b1813
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-06-25 17:02:00 +1000

    Remove useless temporary variable
    The original aim of this was to make implementing thread safety easier, but since there's no thread safety yet it's just confusing.

--- vision-api/src/main/java/me/sparky983/vision/ButtonImpl.java
+++ vision-api/src/main/java/me/sparky983/vision/ButtonImpl.java
@@ -49,14 +49,12 @@ final class ButtonImpl implements Button {
     @Override
     public Button name(final @Nullable Component name) {
 
-        final Component nameToUse;
         if (name != null) {
-            nameToUse = name;
+            this.name = name;
         } else {
-            nameToUse = Component.translatable(type.translationKey());
+            this.name = Component.translatable(type.translationKey());
         }
-        this.name = nameToUse;
-        subscriptionManager.notify((subscriber) -> subscriber.name(nameToUse));
+        subscriptionManager.notify((subscriber) -> subscriber.name(this.name));
         return this;
     }
 
@@ -83,10 +81,8 @@ final class ButtonImpl implements Button {
             Objects.requireNonNull(lore.get(i), "lore[" + i + "] cannot be null");
         }
 
-        final List<Component> loreToUse = List.copyOf(lore);
-
-        this.lore = loreToUse;
-        subscriptionManager.notify((subscriber) -> subscriber.lore(loreToUse));
+        this.lore = List.copyOf(lore);
+        subscriptionManager.notify((subscriber) -> subscriber.lore(this.lore));
         return this;
     }
 
