866baf25ef1f754a89b1864e24aa034398cce004
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2023-08-06 15:34:55 +1000

    Fix memory leak

--- vision-paper/src/main/java/me/sparky983/vision/paper/SubscribingPaperInventoryMirror.java
+++ vision-paper/src/main/java/me/sparky983/vision/paper/SubscribingPaperInventoryMirror.java
@@ -1,5 +1,6 @@
 package me.sparky983.vision.paper;
 
+import me.sparky983.vision.Subscription;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.translation.GlobalTranslator;
 import org.bukkit.Server;
@@ -10,7 +11,9 @@ import org.bukkit.inventory.ItemStack;
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 
@@ -65,12 +68,20 @@ final class SubscribingPaperInventoryMirror implements PaperInventoryMirror {
         final Inventory inventory = new GuiInventoryHolder(gui, inventoryFactory).getInventory();
 
         final Gui.Subscriber subscriber = new Gui.Subscriber() {
+            private final Map<Slot, Subscription> subscriptions = new HashMap<>();
+            
             @Override
             public void button(final Slot slot, final @Nullable Button button) {
+
+                final Subscription subscription = subscriptions.get(slot);
+                if (subscription != null) {
+                    subscription.cancel();
+                }
 
                 final int rawSlot = slot.column() + (slot.row() * gui.columns());
                 if (button == null) {
                     inventory.setItem(rawSlot, null);
+                    subscriptions.remove(slot);
                     return;
                 }
                 final ItemStack item = itemTypeConverter.convert(button.type())
@@ -82,7 +93,7 @@ final class SubscribingPaperInventoryMirror implements PaperInventoryMirror {
                 final ItemStack craftItem = inventory.getItem(rawSlot);
                 // We need to get the item from the inventory because the item is cloned
                 assert craftItem != null;
-                buttonMirror.mirror(button, craftItem, locale);
+                subscriptions.put(slot, buttonMirror.mirror(button, craftItem, locale));
             }
         };
 
